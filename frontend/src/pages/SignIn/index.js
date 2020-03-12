fea215b37d74740533b43c2765182d1cacd4ce49
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-03-12 01:08:37 -0300

    fix: modified login page

---
+++ frontend/src/pages/SignIn/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+// import * as Yup from 'yup';
+import { toast } from 'react-toastify';
+
+import backgroundRocket from '../../assets/rocketmusic.svg';
+
+export default function SignIn() {
+  // Field Validation
+  // const schema = Yup.object().shape({
+  //   email: Yup.string()
+  //     .email()
+  //     .required(),
+  //   password: Yup.string()
+  //     .min(3)
+  //     .required(),
+  // });
+
+  // Functions
+  function handleValidation(fields) {
+    const { email, password } = fields;
+
+    if (email === '') {
+      toast.error('O email é um campo obrigatório!');
+      return false;
+    }
+
+    if (password === '') {
+      toast.error('A senha é um campo obrigatório!');
+      return false;
+    }
+
+    if (password.length < 3) {
+      toast.error('A senha deve ser maior que 3 letras!');
+      return false;
+    }
+
+    return true;
+  }
+
+  function handleSubmit(fields) {
+    if (handleValidation(fields)) {
+      toast.success('Dados válidos');
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <img src={backgroundRocket} alt="Rocket Music" />
+      </div>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          type="email"
+          autoComplete="off"
+          placeholder="Digite seu email"
+        />
+        <Input name="password" type="password" placeholder="Digite sua senha" />
+        <button type="submit">Login</button>
+        <Link to="/register">Criar conta</Link>
+      </Form>
+    </>
+  );
+}
