d01552409e1147d2bf90d27bce86dedf8ab63cf5
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-03-12 01:17:02 -0300

    style: added style for background

--- frontend/src/pages/_layouts/auth/styles.js
+++ frontend/src/pages/_layouts/auth/styles.js
@@ -1,18 +1,49 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { darken } from 'polished';
+
+// import mountainsBackground from '../../../assets/mountains.svg.svg';
+// import starsBackground from '../../../assets/stars.svg';
+
+const backgroundFloating = keyframes`
+  from {
+    transform: translate3d(0, -3px, 0);
+  }
+  to {
+    transform: translate3d(0, 3px, 0);
+  }
+`;
 
 export const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
+  /* flex-direction: column; */
   justify-content: center;
   align-items: center;
   height: 100%;
-  width: 100%;
-  /* background: url(${props => props.background});
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-size: cover;
-  background-attachment: fixed; */
+  /* width: 100%; */
+  background: url('https://skylab.rocketseat.com.br/static/7c60c57e4fd20119d82b5970028b2d1b.svg')
+      center bottom repeat-x,
+    url('https://skylab.rocketseat.com.br/static/d963b8944752859b3c6ccb0bda539c46.svg')
+      repeat-x,
+    rgb(11, 10, 13);
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: right;
+    align-items: center;
+    width: 40%;
+
+    @media (max-width: 1000px) {
+      display: none;
+    }
+
+    img {
+      margin: 0 auto;
+      height: 500px;
+      animation: ${backgroundFloating} 2s infinite alternate;
+    }
+  }
 
   p {
     font-size: 40px;
@@ -27,16 +58,19 @@ export const Container = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    width: 100%;
+    width: 60%;
+
+    @media (max-width: 1000px) {
+      width: 100%;
+    }
 
     input {
-      width: 30%;
+      width: 60%;
       padding: 15px 20px;
       color: #999;
       letter-spacing: 0.8px;
-      background: #f0f0f3;
-      box-shadow: 10px 10px 30px rgba(174, 174, 192, 0.4),
-        -10px -10px 30px #ffffff;
+      background: #19181f;
+      box-shadow: 5px 5px 5px #28272c, -5px -5px 5px #19181f;
       border-radius: 10px;
 
       & + input {
@@ -51,28 +85,13 @@ export const Container = styled.div`
 
     button {
       margin-top: 35px;
-      width: 10%;
+      width: 20%;
       padding: 15px 20px;
       font-weight: bold;
-      color: #7159c1;
-      background: #f0f0f3;
-      box-shadow: 7px 7px 15px rgba(55, 84, 170, 0.15),
-        -7px -7px 20px rgba(255, 255, 255, 1),
-        inset 0px 0px 4px rgba(255, 255, 255, 0.2),
-        inset 7px 7px 15px rgba(55, 84, 170, 0),
-        inset -7px -7px 20px rgba(255, 255, 255, 0),
-        0px 0px 4px rgba(255, 255, 255, 0) !important;
-      border-radius: 10px;
-      transition: box-shadow 0.25s ease !important;
-
-      &:active {
-        box-shadow: 7px 7px 15px rgba(55, 84, 170, 0.15),
-          -7px -7px 20px rgba(255, 255, 255, 1),
-          inset 0px 0px 4px rgba(255, 255, 255, 0),
-          inset 7px 7px 15px rgba(55, 84, 170, 0.15),
-          inset -7px -7px 20px rgba(255, 255, 255, 1),
-          0px 0px 4px rgba(255, 255, 255, 0.2) !important;
-      }
+      color: #fff;
+      background: #7159c1;
+      border-radius: 5px;
+      text-transform: uppercase;
     }
   }
 
