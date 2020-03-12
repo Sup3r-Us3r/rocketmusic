54b0e102bf89af450f3abf28f20dc19f32f5da9f
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-03-12 01:13:02 -0300

    fix: background property removed

--- frontend/src/pages/_layouts/auth/index.js
+++ frontend/src/pages/_layouts/auth/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import background from '../../../assets/background-auth-layout.svg';
-
 import { Container } from './styles';
 
 export default function AuthLayout({ children }) {
-  return <Container background={background}>{children}</Container>;
+  return <Container>{children}</Container>;
 }
 
 AuthLayout.propTypes = {
