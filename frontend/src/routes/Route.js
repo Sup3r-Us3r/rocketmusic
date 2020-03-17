0ed54856dcbef61515024ae75af3500bef2960d0
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-03-16 23:13:41 -0300

    change value

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -12,7 +12,7 @@ export default function RouteWrapper({
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const signed = true;
 
   if (!signed && isPrivate) {
     return <Redirect to="/login" />;
