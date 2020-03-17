28f0adc81772fbe1e31b0d693c5414cfdc2d5949
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-03-16 22:58:41 -0300

    style: formatted code

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,37 +2,37 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    jest: true,
+    jest: true
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    __DEV__: true,
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    __DEV__: true
   },
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: ['react', 'jsx-a11y', 'import', 'prettier', 'react-hooks'],
+  plugins: ["react", "jsx-a11y", "import", "prettier", "react-hooks"],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
-    'import/prefer-default-export': 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'react/jsx-one-expression-per-line': 'off',
-    'global-require': 'off',
-    'react-native/no-raw-text': 'off',
-    'no-param-reassign': 'off',
-    'no-underscore-dangle': 'off',
-    camelcase: 'off',
-    'no-console': ['error', { allow: ['tron'] }],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/jsx-props-no-spreading': 'off',
-  },
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", ".js"] }],
+    "import/prefer-default-export": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "react/jsx-one-expression-per-line": "off",
+    "global-require": "off",
+    "react-native/no-raw-text": "off",
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    camelcase: "off",
+    "no-console": ["error", { allow: ["tron"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-props-no-spreading": "off"
+  }
 };
