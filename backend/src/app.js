ea6c3deeee7dc8a83096ac82c37d5b15f635c657
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-03-16 22:46:07 -0300

    cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express from 'express';
+import cors from 'cors';
 
 import routes from './router';
 
@@ -16,6 +17,7 @@ class App {
   middlewares() {
     this.server.use(express.urlencoded({ extended: false }));
     this.server.use(express.json());
+    this.server.use(cors());
   }
 
   routes() {
