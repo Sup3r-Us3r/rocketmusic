f381304d352e563a99faad607045c35648d2e628
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-03-16 23:11:26 -0300

    style: added styles for component

---
+++ frontend/src/pages/SendMusic/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  form {
+    width: 70%;
+
+    input {
+      margin: 5px;
+    }
+  }
+`;
