e8d5528ef7d334a8312de0b814318a9396e0cf4e
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-04-11 12:18:11 -0300

    added redux to share music state

--- frontend/src/components/Player/index.js
+++ frontend/src/components/Player/index.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import {
   MdPlayArrow,
   MdPause,
   MdSkipPrevious,
   MdSkipNext,
   MdVolumeUp,
-  MdVolumeMute,
+  MdVolumeOff,
 } from 'react-icons/md';
 
 import borderLeft from '../../assets/border-left.svg';
 import borderRight from '../../assets/border-right.svg';
-
-import musicPath from '../../assets/Eminem-Not-Afraid.mp3';
 
 import {
   Container,
@@ -23,8 +22,12 @@ import {
   VolumeProgress,
 } from './styles';
 
+import api from '../../services/api';
+
 export default function Player() {
-  // States
+  // REDUX - STATE
+  const musicInfo = useSelector(state => state.play.musicInfo);
+
   const [audio, setAudio] = useState(new Audio());
   const [playOrPause, setPlayOrPause] = useState(false);
   const [muteOrUnmuted, setMuteOrUnmuted] = useState(false);
@@ -33,7 +36,6 @@ export default function Player() {
   const [volume, setVolume] = useState(100);
   const [currentTimeAudio, setCurrentTimeAudio] = useState('');
 
-  // Functions
   function handleBeautifulTime(time) {
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
@@ -41,21 +43,15 @@ export default function Player() {
     return `${`0${minutes}`.slice(-2)}:${`0${seconds}`.slice(-2)}`;
   }
 
-  function handlePlayOrPause() {
-    const audioPromise = audio.play();
+  async function handlePlayOrPause() {
+    await audio.play();
 
-    if (audioPromise !== undefined) {
-      audioPromise
-        .then(() => {
-          if (playOrPause) {
-            setPlayOrPause(false);
-            audio.pause();
-          } else {
-            setPlayOrPause(true);
-            audio.play();
-          }
-        })
-        .catch(err => err);
+    if (playOrPause) {
+      setPlayOrPause(false);
+      audio.pause();
+    } else {
+      setPlayOrPause(true);
+      audio.play();
     }
   }
 
@@ -83,10 +79,19 @@ export default function Player() {
     return duration;
   }
 
-  // Effects
   useEffect(() => {
-    function handleInitMusicData() {
-      audio.src = musicPath;
+    async function handleInitMusicData() {
+      const music = await api.get(`/musics/${musicInfo.id}`, {
+        responseType: 'blob',
+      });
+
+      const fileReader = new FileReader();
+
+      fileReader.readAsDataURL(music.data);
+      fileReader.onloadend = () => {
+        audio.src = fileReader.result;
+      };
+
       audio.load();
 
       audio.onloadeddata = () => {
@@ -100,7 +105,7 @@ export default function Player() {
     }
 
     handleInitMusicData();
-  }, [audio]);
+  }, [audio, musicInfo]);
 
   return (
     <Container>
@@ -111,10 +116,10 @@ export default function Player() {
 
       <PlayerMusic>
         <MusicInfo>
-          <img src="https://i.snipboard.io/lt8WpB.jpg" alt="Cover" />
+          <img src={musicInfo.cover} alt="Cover" />
           <div>
-            <p>Not Afraid</p>
-            <p>Eminem, 2010</p>
+            <p>{musicInfo.name}</p>
+            <p>{musicInfo.author}</p>
           </div>
         </MusicInfo>
         <PlayerFunctions>
@@ -129,14 +134,14 @@ export default function Player() {
           </section>
           <section id="progress-bars">
             <MusicProgress>
-              <time>{currentTimeAudio}</time>
+              <time>{currentTimeAudio || '00:00'}</time>
               <input
                 type="range"
                 min="0"
                 max={progressBarMax}
                 step="1"
-                onInput={e => handleMusicProgress(e)}
-                onChange={e => handleMusicProgress(e)}
+                onInput={handleMusicProgress}
+                onChange={handleMusicProgress}
                 value={progressBar}
               />
               <time>{handleAudioDuration()}</time>
@@ -150,7 +155,7 @@ export default function Player() {
                   onClick={handleMuteOrUnmuted}
                 />
               ) : (
-                <MdVolumeMute
+                <MdVolumeOff
                   size={30}
                   color="#fff"
                   onClick={handleMuteOrUnmuted}
@@ -161,8 +166,8 @@ export default function Player() {
                 min="0"
                 max="100"
                 step="1"
-                onInput={e => handleVolumeProgress(e)}
-                onChange={e => handleVolumeProgress(e)}
+                onInput={handleVolumeProgress}
+                onChange={handleVolumeProgress}
                 value={volume}
               />
             </VolumeProgress>
@@ -172,12 +177,3 @@ export default function Player() {
     </Container>
   );
 }
-
-/**
- * <MdPause size={30} color="#fff" />
- * <MdStop size={30} color="#fff" />
- * <MdVolumeOff size={30} color="#fff" />
- * <MdVolumeDown size={30} color="#fff" />
- * <MdVolumeMute size={30} color="#fff" />
- * <MdRepeatOne size={30} color="#fff" />
- */
