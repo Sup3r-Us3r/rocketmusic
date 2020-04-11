9a3657c92bca0505c08dcb371f7023a7b8166f8d
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-04-11 12:24:12 -0300

    listen to actions and modify state

--- frontend/src/store/modules/play/reducer.js
+++ frontend/src/store/modules/play/reducer.js
@@ -1,14 +1,17 @@
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  play: 0,
+  play: false,
 };
 
 export default function play(state = INITIAL_STATE, action) {
   return produce(state, draft => {
     switch (action.type) {
-      case '@play/PLAYING_REQUEST': {
-        draft.play = 1;
+      case '@play/SEND_MUSIC_INFO_SUCCESS': {
+        const { musicInfo } = action.payload;
+
+        draft.play = true;
+        draft.musicInfo = musicInfo;
         break;
       }
       default:
