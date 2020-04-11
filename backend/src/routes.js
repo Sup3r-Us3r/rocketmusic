18fbe6409f0c376ae94cd07d32a030fedb5abb6d
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-04-11 12:13:59 -0300

    renamed file

---
+++ backend/src/routes.js
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/User';
+import MusicController from './app/controllers/Music';
+
+import AuthMiddleware from './app/middlewares/Auth';
+
+const routes = Router();
+
+// User Routes
+routes.get('/users', AuthMiddleware, UserController.index);
+routes.post('/register', UserController.store);
+routes.post('/login', UserController.show);
+routes.delete('/delete/:id', UserController.destroy);
+
+// Music Routes
+routes.get('/musics', MusicController.listMusics);
+routes.get('/musics/:id', MusicController.listMusic);
+routes.get('/musics/info/:id', MusicController.listMusicInfo);
+routes.post(
+  '/sendmusic',
+  multer(multerConfig).single('sendMusic'),
+  MusicController.sendMusic
+);
+routes.post('/sendmusicinfo', MusicController.sendMusicInfo);
+
+export default routes;
