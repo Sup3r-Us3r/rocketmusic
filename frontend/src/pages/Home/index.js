5123d3d3d460d344661031513b54f367a75fd2af
Sup3r-Us3r <pc.gam3rs.tuto@gmail.com>
2020-04-11 12:20:59 -0300

    added function to play music

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,47 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { MdPlayCircleOutline, MdPauseCircleOutline } from 'react-icons/md';
 
 import Header from '../../components/Header';
 
+import { sendMusicInfoRequest } from '../../store/modules/play/actions';
+
+import api from '../../services/api';
+
 import { Container, Content, Music } from './styles';
 
 export default function Home() {
-  // States
-  const [playAndPause, setPlayAndPause] = useState(true);
-  const [musicImage, setMusicImage] = useState(
-    'https://i.snipboard.io/lt8WpB.jpg'
-  );
+  // REDUX
+  const dispatch = useDispatch();
 
-  // Functions
-  function handlePlayAndPause() {
-    setPlayAndPause(!playAndPause);
+  const [songs, setSongs] = useState([]);
+  const [musicId, setMusicId] = useState('');
+
+  function handlePlayOrPause(id) {
+    setMusicId(id);
+
+    if (musicId === id) {
+      setMusicId('');
+    } else {
+      dispatch(sendMusicInfoRequest(id));
+    }
   }
+
+  useEffect(() => {
+    async function handleListMusics() {
+      const musics = await api.get('/musics');
+
+      setSongs(musics.data);
+    }
+
+    handleListMusics();
+  }, []);
 
   return (
     <Container>
       <Header />
       <h1>Descobrir</h1>
       <Content>
-        <Music musicImage={musicImage}>
-          <div>
-            <div />
-            {playAndPause ? (
-              <MdPlayCircleOutline
-                size={80}
-                color="#fff"
-                onClick={handlePlayAndPause}
-              />
-            ) : (
-              <MdPauseCircleOutline
-                size={80}
-                color="#fff"
-                onClick={handlePlayAndPause}
-              />
-            )}
-            <h1>Hip-Hop/Rap</h1>
-            <span>Eminem</span>
-          </div>
-        </Music>
+        {songs.map(music => (
+          <Music key={music._id}>
+            <div>
+              <img src={music.cover} alt="Cover" />
+              {musicId !== music._id ? (
+                <MdPlayCircleOutline
+                  size={80}
+                  color="#fff"
+                  onClick={() => handlePlayOrPause(music._id)}
+                />
+              ) : (
+                <MdPauseCircleOutline
+                  size={80}
+                  color="#fff"
+                  onClick={() => handlePlayOrPause(music._id)}
+                />
+              )}
+              <h1>{music.author}</h1>
+              <span>{music.gender}</span>
+            </div>
+          </Music>
+        ))}
       </Content>
     </Container>
   );
